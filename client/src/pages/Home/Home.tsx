7e727e6ffa3aacfe52ae84692aedea2fd723a7eb
Jackson Furr <jackson.a.furr@gmail.com>
2024-02-15 17:51:20 -0500

    add fragrances (fe-only)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./Home.module.scss";
 
 // Comps
@@ -15,25 +15,73 @@ type IProps = {
     setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+const TEST_LIST = [
+    "Chanel No. 5",
+    "Dior Sauvage",
+    "Jo Malone London English Pear",
+    "Yves Saint Laurent Black Opium",
+    "Creed Aventus",
+    "Gucci Bloom",
+    "Tom Ford Black Orchid",
+    "Viktor & Rolf Flowerbomb",
+    "Marc Jacobs Daisy",
+    "Prada Candy",
+];
+
 const Home: React.FC<IProps> = ({
     loggedIn,
     validationCompleted,
     setUserData,
     setLoggedIn,
 }) => {
+    const [fragrances, setFragrances] = useState<string[]>([...TEST_LIST]);
+    const [input, setInput] = useState<string>("");
+
+    function addFragrance() {
+        const newFragrances = [...fragrances];
+        newFragrances.push(input);
+        setFragrances(newFragrances);
+    }
+
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+        setInput(e.target.value.toString());
+    }
+
     return (
         <Protected
             validationCompleted={validationCompleted}
             loggedIn={loggedIn}
         >
-            <>
-                <h1>My Fragances</h1>
-                <LogoutBtn
-                    setUserData={setUserData}
-                    setLoggedIn={setLoggedIn}
-                />
-                <div className={styles.test}></div>
-            </>
+            <div className={styles.root}>
+                <section className={styles.title}>
+                    <h1>My Fragances</h1>
+                </section>
+
+                <div className={styles.main}>
+                    <section className={styles.owned}>
+                        <h2>Owned</h2>
+                        <div>
+                            {fragrances.map((name: string, i: number) => {
+                                return <div key={i.toString()}>{name}</div>;
+                            })}
+                        </div>
+                    </section>
+
+                    <section className={styles.add}>
+                        <h2>Add</h2>
+                        <div>
+                            <input
+                                type="text"
+                                name="fragrance"
+                                id="fragrance"
+                                onChange={handleInput}
+                                value={input}
+                            />
+                            <button onClick={addFragrance}>Add</button>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </Protected>
     );
 };
